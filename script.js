8c3bf7e302a8df824b8ed4cc04f9a62ad98cd84a
bayana7med <layan7amed25@gmail.com>
2024-03-04 22:24:51 +0200

    new commit

--- script.js
+++
@@ -1,8 +0,0 @@
-// توليد رقم عشوائي
-let randomNumber = Math.random();
-
-// تحويل الرقم إلى نص وعرضه باللغة العربية
-let arabicNumber = randomNumber.toLocaleString('ar-EG');
-
-// عرض الرقم
-console.log(arabicNumber);
